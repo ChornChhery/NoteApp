a91acb4366893affcfe03f425a5938d224b03c5d
Chhery Chorn <chherychorn@gmail.com>
2026-03-26 10:06:10 +0700

    Add user model'

---
+++ backend/Models/User.cs
@@ -0,0 +1,22 @@
+namespace backend.Models;
+
+public class User
+{
+    public int Id {get; set;}
+    public string Username {get; set;} = "";
+    public string Email {get; set;} = "";   
+    public string Password {get; set;} = "";
+}
+
+public class RegisterDto
+{
+    public string Username {get; set;} = "";
+    public string Email {get; set;} = "";   
+    public string Password {get; set;} = "";
+}
+
+public class LoginDto
+{
+    public string Email {get; set;} = "";   
+    public string Password {get; set;} = "";
+}
